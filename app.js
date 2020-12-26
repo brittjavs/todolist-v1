25d94b1d8ab00cb9eeff48cb72c464f36928e5f5
Brittany Javalera <brittjavalera@gmail.com>
2020-12-26 12:03:35 -0800

    install ejs, set app to use ejs. Use ejs to send day variable to new list.ejs file

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.sendStatus("Hello");
+   var today = new Date();
+   var currentDay = today.getDay()
+   var day = "";
+   if(currentDay === 6 || currentDay === 0){
+       day = "Weekend"
+       res.render("list", {kindOfDay: day});
+   }
+   else{
+       res.sendFile(__dirname + "/index.html");
+   }
 });
 
 app.listen(3000, function(){
