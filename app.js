1bcf1e969df92853c1c8d1235df3f3bbbff66c26
Brittany Javalera <brittjavalera@gmail.com>
2020-12-26 11:29:59 -0800

    installed npm, express, bodyParser and required them in app.js.

---
+++ app.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendStatus("Hello");
+});
+
+app.listen(3000, function(){
+    console.log("Server has started on port 3000");
+});
