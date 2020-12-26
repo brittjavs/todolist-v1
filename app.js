88b7e02574174df47b3a04f291e2ac47cdae1565
Brittany Javalera <brittjavalera@gmail.com>
2020-12-26 12:33:38 -0800

    revise day variable in app.js to use options variable in toLocaleDateString. revise list.ejs html to display updated  kindOfDay

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
    let today = new Date();
-   let currentDay = today.getDay()
-   let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-   let day = daysOfWeek[currentDay];
-   
-   res.render("list", {kindOfDay: day});
+   let options = {
+       weekday: "long",
+       day: "numeric",
+       month: "long"
+   };
+
+   let day = today.toLocaleDateString("en-US", options);
+
+   res.render("list", {kindOfDay: day})
+
 });
 
 app.listen(3000, function(){
