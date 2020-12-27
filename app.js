84be82bd06355c118e556a38a057d7ac0874e14c
Brittany Javalera <brittjavalera@gmail.com>
2020-12-27 12:11:19 -0800

    refactor code to remove get date funcitonality from inside app.get and move to a module. require module and call it in app.get.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js")
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,19 +11,11 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static("public"))
 
-let tasks = ["Make Dinner", "Clean the Kitchen"];
-let workTasks = []
+const tasks = ["Make Dinner", "Clean the Kitchen"];
+const workTasks = []
 
 app.get("/", function(req, res){
-   let today = new Date();
-   let options = {
-       weekday: "long",
-       day: "numeric",
-       month: "long"
-   };
-
-   let day = today.toLocaleDateString("en-US", options);
-
+   let day = date.getDate()
    res.render("list", {listTitle: day, newListItems: tasks})
 
 });
